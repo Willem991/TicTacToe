ad73f87a97e3d2eb35222ac082eca7a849aa3295
Willem Ruan <ruan.nieu@gmail.com>
2021-08-25 16:03:55 +0200

    added check for win and other methods

--- script.js
+++ script.js
@@ -7,32 +7,70 @@ const player = (name, type) => {
 
 const gameBoard = (() => {
 
-    let boardArray = ["x","o","x","o","x","o","x","o","x"];
+    let boardArray = ["x","x","x","o","x","o","x","o","x"];
 
-    return({boardArray});
+    const updateBoardArray = (type, id) => {
+        boardArray[id] = type;
+    };
+
+    const resetBoardArray = () => {
+        boardArray = [];
+    };
+
+    const checkForWin = () => {
+        
+        for(let i = 0; i <= 3; i++){
+            if(boardArray[i] == boardArray[i+3] && boardArray[i] == boardArray[i+6] && boardArray[i] != ""){
+                return [true, boardArray[i]]
+            }else if(boardArray[i] == boardArray[i+1] && boardArray[i] == boardArray[i+2] && boardArray[i] != ""){
+                return [true, boardArray[i]]
+            }else if(i != 1 && boardArray[i] == boardArray[5] && boardArray[i] == boardArray[i+8] && boardArray[i] != ""){
+                return [true, boardArray[i]]
+            }else{
+                return false
+            };
+        };
+    };
+
+    return({boardArray, updateBoardArray, resetBoardArray,checkForWin});
 })();
 
 const displayScreen = (() =>{
     
     const writesquare = (array) =>{
-        const blocks = document.querySelectorAll(".block");
+
+        let blocks = document.querySelectorAll(".block");
+        
         let divCounter = 0;
 
         blocks.forEach(element => {
 
             if(array[divCounter] == "x"){
-                element.style.backgroundColor = "red";
+                element.classList.add('x');
+
             }else{
-                element.style.backgroundColor = "blue";
+                element.classList.add('o');
+                console.log(element);
             }
 
             divCounter++;
         });
     };
 
-    return({writesquare})
+    const clearScreen = () => {
+
+        let blocks = document.querySelectorAll(".block");
+
+        blocks.forEach(element => {
+            element.classList.remove('o');
+            element.classList.remove('x');
+        });
+    };
+
+    return({writesquare, clearScreen})
 })();
 
 const botAI = (() => {
 
 })();
+
