7e212bc8df07b02663657c1c666a75004ef0da2a
Willem Ruan <ruan.nieu@gmail.com>
2021-08-25 18:53:12 +0200

    added form controller module

--- script.js
+++ script.js
@@ -67,8 +67,62 @@ const displayScreen = (() =>{
         });
     };
 
-    return({writesquare, clearScreen})
+    const start = () => {
+        const boardDisplay = document.querySelector('#gameBoard');
+        const characterScreen = document.querySelector('#startPage');
+
+        boardDisplay.classList.add("active");
+        boardDisplay.classList.remove("inactive");
+        characterScreen.classList.add("inactive");
+        characterScreen.classList.remove("active");
+
+    }
+
+    const reset = () =>{
+        boardDisplay.classList.add("inactive");
+        boardDisplay.classList.remove("active");
+        characterScreen.classList.add("active");
+        characterScreen.classList.remove("inactive");
+    }
+
+    return({writesquare, clearScreen, start, reset})
 })();
+
+const formController = (() => {
+    const playerBtn = document.querySelector("#selectAI");
+    const typeBtn = document.querySelector("#selectType");
+    const startBtn = document.querySelector('#start');
+    let playerStatus = "player";
+    let typeStatus = "x";
+
+    playerBtn.addEventListener('click',() => {
+        if(playerStatus == "player"){
+            playerStatus = "AI";
+            playerBtn.textContent = "AI";
+        }else{
+            playerStatus = "player";
+            playerBtn.textContent = "Player";
+        };
+    });
+
+    typeBtn.addEventListener('click',() => {
+        if(typeStatus == "x"){
+            typeStatus = "o";
+            typeBtn.textContent = "O";
+        }else{
+            typeStatus = "x";
+            typeBtn.textContent = "X";
+        };
+    });
+
+    startBtn.addEventListener('click', () => {
+        displayScreen.start();
+    });
+
+    return({playerStatus, typeStatus})
+
+})();
+
 
 const botAI = (() => {
 
